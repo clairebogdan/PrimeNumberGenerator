66011ec1b2be3107a383a62d74bfdd17dc26b80c
clairebogdan <bogdanclaire@gmail.com>
2020-09-21 22:54:56 -0400

    Deleted unused code

--- test/primenumbergenerator/PrimeListTest.java
+++ test/primenumbergenerator/PrimeListTest.java
@@ -2,34 +2,11 @@ package primenumbergenerator;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PrimeListTest {
     
-    public PrimeListTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
     // generate() Test
     @Test
     public void testGenerate() {
